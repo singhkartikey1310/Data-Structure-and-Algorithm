35830040f88daff9560c8af237ab9d5f10441e0c
singhkartikey1310 <kartikeydev05@gmail.com>
2026-05-18 12:36:41 +0530

    Hello JAVA

---
+++ Introduction to Java/hello.java
@@ -0,0 +1,11 @@
+public class hello {
+    public static void main (String[] args){
+        int age = 26;
+        int marks = 44;
+        char myletter = 'P';
+        System.out.println(age);
+        System.out.println(marks);
+        System.out.println(myletter);
+
+    }
+}
